4aa887ba3a797245e3b37d2278a1132b3c1b7c3e
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-17 23:44:31 +0530

    Add files via upload

---
+++ statistics Assignment 2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1.How can we figure out what the interquartile range is?
+# 
+# Ans:The IQR describes the middle 50% of values when ordered from lowest to highest.
+# To find the interquartile range (IQR), 
+# ​first find the median (middle value) of the lower and upper half of the data.
+# These values are quartile 1 (Q1) and quartile 3 (Q3). 
+# The IQR is the difference between Q3 and Q1.
+
+# Question 2. What exactly is the value of the 5-number theory?
+# 
+# Ans: Reporting five numbers avoids the need to decide on the 
+# most appropriate summary statistic.
+# The five-number summary gives information about the location (from the median),
+# spread (from the quartiles) and range (from the sample minimum and maximum) of 
+# the observations.
+
+# Question 3.What is the relationship between standard deviation and variance?
+# 
+# Ans: Variance is the square of standard deviation.
+# Both variance and standard deviation are always positive. If all the observations in a data set are identical, then the standard deviation and variance will be zero.
+
+# Question 4.What does the difference between variance and standard deviation mean?
+# 
+# Ans:Standard deviation is the spread of a group of numbers from the mean.
+# The variance measures the average degree to which each point differs from the mean. 
+# While standard deviation is the square root of the variance, 
+# variance is the average of all data points within a group. 
+
+# Question 5.When is it appropriate to refer to a skewed data distribution?
+# 
+# Ans:A skewed distribution is neither symmetric nor normal because
+# the data values trail off more sharply on one side than on the other. 
+# In business, you often find skewness in data sets that represent sizes
+# using positive numbers (eg, sales or assets).
+
+# In[ ]:
+
+
+
+
