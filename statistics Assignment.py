3ebcc7b54102783bf044ce717a92ac8ac2e752d3
jainaman0327 <96785277+jainaman0327@users.noreply.github.com>
2022-07-17 23:43:07 +0530

    Add files via upload

---
+++ statistics Assignment.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1.What exactly is the difference between descriptive and inferential statistics?
+# 
+# Ans: Descriptive statistics summarize the characteristics of a data set. Inferential statistics allow you to test a hypothesis or assess whether your data is generalizable to the broader population.
+
+# Question 2. I'm not sure what is the difference between a sample and a population?
+# 
+# Ans: A population is the entire group that you want to draw conclusions about.
+# 
+# A sample is the specific group that you will collect data from. The size of the sample is always less than the total size of the population.
+# 
+# In research, a population doesn’t always refer to people. It can mean a group containing elements of anything you want to study, such as objects, events, organizations, countries, species, organisms, etc.
+
+# Question 3.What distinguishes descriptive statistics from other types of statistics?
+# 
+# Ans:Descriptive statistics summarize the characteristics of a data set. 
+# Inferential statistics allow you to test a hypothesis or assess whether your data is
+# generalizable to the broader population.
+
+# Question 4. What is the difference between quantitative and qualitative data?
+# 
+# Ans: Quantitative data is numbers-based, countable, or measurable.
+# Qualitative data is interpretation-based, descriptive, and relating to language.
+# Quantitative data tells us how many, how much, or how often in calculations.
+# Qualitative data can help us to understand why, how, or what happened behind certain behaviors.
+
+# Question 5.What is the definition of a percentile?
+# 
+# Ans: A percentile (or a centile) is a measure used in
+# statistics indicating the value below which a given percentage 
+# of observations in a group of observations fall. For example, 
+# the 20th percentile is the value (or score) below which 20% of the observations may be found.
+# 
+# The term percentile and the related term percentile rank are often used in the reporting of scores
+# from norm-referenced tests. For example, if a score is at the 86th percentile, where 86 is the percentile rank, 
+# it is equal to the value below which 86% of the observations may be found. In contrast, if it is in the 86th percentile,
+# the score is at or below the value of which 86% of the observations may be found. Every score is in the 100th percentile.
+# The 25th percentile is also known as the first quartile (Q1), the 50th percentile as the median or second quartile (Q2),
+# and the 75th percentile as the third quartile (Q3). In general, percentiles and quartiles are specific types of quantiles.
+
+# In[ ]:
+
+
+
+
